b39340917a32d1f57263464a2dab738e5dbf587c
badaueba <alan.alalabadaueba@hotmail.com>
2020-02-14 16:23:51 -0300

    using dotenv to store env vars

--- config/mongooseURI.js
+++ config/mongooseURI.js
@@ -1,3 +1,2 @@
 const mongoose = require("mongoose");
-
 module.exports = `mongodb+srv://${process.env.database_user}:${process.env.database_pass}@cluster0-5pniq.mongodb.net/${process.env.database_name}`;
